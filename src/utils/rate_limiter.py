ea78f90cb0b2582da2f777f3039dc4d56921967f
aywil <imaybedeadz@gmail.com>
2025-12-23 19:45:28 +0700

    create class RateLimiter

---
+++ src/utils/rate_limiter.py
@@ -0,0 +1,53 @@
+import random
+from time import time
+from redis import Redis
+
+
+class RateLimiter:
+    def __init__(self, redis: Redis):
+        self._redis = redis
+
+    async def is_limited(
+        self,
+        ip_address: str,
+        endpoint: str,
+        max_requests: int,
+        window_seconds: int,
+    ) -> bool:
+        key = f"rate_limiter:{endpoint}:{ip_address}"
+
+        current_ms = time() * 1000
+        window_start_ms = current_ms - window_seconds * 1000
+        current_ruquest = f"{time() * 1000} - {random.randint(0, 100_000)}"
+
+        async with self._redis.pipeline() as pipe:
+            await pipe.zremrangebyscore(
+                name=key,
+                min=0,
+                max=window_start_ms,
+            )
+
+            await pipe.zcard(
+                name=key,
+            )
+
+            await pipe.zadd(
+                key,
+                mapping={current_ruquest: current_ms},
+            )
+
+            await pipe.expire(
+                name=key,
+                time=window_seconds,
+            )
+
+            res = await pipe.execute()
+
+        (
+            _,
+            current_count,
+            _,
+            _,
+        ) = res
+
+        return current_count >= max_requests
