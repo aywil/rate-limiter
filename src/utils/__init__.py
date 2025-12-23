c33124e824dcd41e50ada3a88287dd718bb712c4
aywil <imaybedeadz@gmail.com>
2025-12-23 20:15:34 +0700

    create LimitedRateFactory + add imports

---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "rate_limiter_factory",
+    "rate_limit_lite",
+    "rate_limit_hard",
+    "RateLimiter",
+)
+
+from .limiter_factory import rate_limiter_factory
+from .rate_limiter import RateLimiter
---
+++ src/utils/limiter_factory.py
@@ -0,0 +1,35 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Request, status
+from dependencies import get_rate_limiter
+from utils.rate_limiter import RateLimiter
+
+
+def rate_limiter_factory(
+    endpoint: str,
+    max_requests: int,
+    window_seconds: int,
+):
+    async def dependency(
+        request: Request,
+        rate_limiter: Annotated[
+            RateLimiter,
+            Depends(get_rate_limiter),
+        ],
+    ):
+        ip_address = request.client.host
+
+        limited = await rate_limiter.is_limited(
+            ip_address,
+            endpoint,
+            max_requests,
+            window_seconds,
+        )
+
+        if limited:
+            raise HTTPException(
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                detail="Too many requests",
+            )
+
+    return dependency
